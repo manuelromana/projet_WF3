873a4eb50c90f5bcbdd73017f22986ca9551cd36
manuelromana <mr.romanamanuel@gmail.com>
2021-01-18 07:28:31 -0500

    new script and change app name in docker compose

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-cd flask_app
-docker build . -t flask_application
-cd ../nginx
-docker build . -t my_nginx
+cd flask_app/
+docker build . -t app_1:1.0
+cd ../nginx/
+docker build . -t my_nginx:1.0
 cd ..
 docker-compose up
