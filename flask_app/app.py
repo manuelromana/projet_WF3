e06ea1bea852933ca9ef77523f1ac65fa7469805
manuelromana <mr.romanamanuel@gmail.com>
2021-01-18 10:03:38 -0500

    add formulaire

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask ,render_template,request
 app = Flask(__name__)
 
 @app.route('/')
 def app_root():
-    return os.environ.get('APP_NAME', 'no app name')
+    return os.environ.get('PATH', 'no app name')
+
+@app.route('/formulaire')
+def index():
+  return render_template("index.html")
+
+@app.route('/resultat',methods = ['POST'])
+def resultat():
+  result = request.form
+  n = result['nom']
+  p = result['prenom']
+  return render_template("resultat.html", nom=n, prenom=p)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
